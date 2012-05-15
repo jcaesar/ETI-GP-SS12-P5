67fe61fad32909c9cdf00f68beb9f29c16493652
julius <julius@2d2cb452-3308-4e9a-bf1f-7df7ff3b6518>
2012-05-15 13:20:47 +0000

    init.sh enhancement
    git-svn-id: http://doppelklick.dyndns.org/etigp/trunk@6 2d2cb452-3308-4e9a-bf1f-7df7ff3b6518

--- init.sh
+++ init.sh
@@ -45,6 +45,7 @@ else
 	rm valgrind-3.7.0.tar.bz2
 	mv valgrind-3.7.0 $VALGRIND_SRC
 fi
+
 if [[ -x $VALGRIND_INSTALL/bin/valgrind && ( -x $VALGRIND_SRC/mctracer/mctracer-amd64-linux || -x $VALGRIND_SRC/mctracer/mctracer-x86-linux ) ]]; then
 	echo "valgrind found installed, mctracer present." | tee -a $BUILDLOG
 else
@@ -90,25 +91,51 @@ else
 			exit
 		fi
 	fi
-	echo "building valgrind..." | tee -a $BUILDLOG
+	echo -n "building valgrind... " | tee -a $BUILDLOG
 	make -j 8 &>>$BUILDLOG # 8 is an a lot saner number than what plain -j does
 	if [[ $? -ne 0 ]]; then
-		echo "Error in build progress" | tee -a $BUILDLOG
+		echo "failed, cleaning up and exiting" | tee -a $BUILDLOG
+		make clean &>>$BUILDLOG
 		cd $START_DIR
 		exit
+	else
+		echo "success"
 	fi
-	echo "installing valgrind..."  | tee -a $BUILDLOG
+	echo -n "installing valgrind... "  | tee -a $BUILDLOG
 	make install &>>$BUILDLOG
+	if [[ $? -eq 0 ]]; then
+		echo "success"
+	else
+		echo "failed"
+	fi
 	cd $START_DIR
 fi
 cd $START_DIR
+
+
 cd simplesim
-echo "making simplesim..."  | tee -a $BUILDLOG
+echo -n "making simplesim... "  | tee -a $BUILDLOG
 make &>>$BUILDLOG
+if [[ $? -eq 0 ]]; then
+	echo "success";
+else
+	echo "failed, cleaning up..."
+	make clean all &>> $BUILDLOG
+fi
+
+
 cd ../example
-echo "making example..."  | tee -a $BUILDLOG
+echo -n "making example... "  | tee -a $BUILDLOG
 make &>>$BUILDLOG
+if [[ $? -eq 0 ]]; then
+	echo "success";
+else
+	echo "failed, cleaning up..."
+	make clean all &>> $BUILDLOG
+fi
 cd $START_DIR
+
+
 PATH_ADDITION=$VALGRIND_INSTALL/bin/:$(cd $VALGRIND_SRC; pwd)/mctracer
 if [[ ! $PATH == *"$PATH_ADDITION"* ]]; then
 	echo "To use valgrind, do $ export PATH=\$PATH:$PATH_ADDITION"  | tee -a $BUILDLOG
